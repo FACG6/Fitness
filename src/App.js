5ea99b83d93bd478eb9d62683bd244cc7e8f4e3e
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-06 23:05:18 +0300

    install pakage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-import TodayWorkout from "./components/TodayWorkout/TodayWorkout";
+import React, {
+  Component
+} from 'react';
 import './App.css';
+import Login from './components/Login';
+import Home from './components/TodayWorkout/TodayWorkout';
+import Edit from './components/Edit'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TodayWorkout />
-      </div>
-    );
-  }
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() { 
+  const email = localStorage.getItem('email');
+    const isLogged = email==='fatma.o.siam@gmail.com';
+      return (
+        <Router>
+          <Route path="/login" exact render={() => (!isLogged ? <Login /> : <Home />)} />
+          <Route path='/'   exact  render={()=>(isLogged ? <Home /> : <Login /> )}/>
+          <Route path='/edit'  exact render={()=>(isLogged ? <Edit /> : <Login />)}/>
+        </Router>
+      )  
+  
 }
 
 export default App;
