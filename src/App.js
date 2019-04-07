7804c18889d3dcada87410fb16f3b8537382cbf5
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-07 09:27:24 +0300

    remove unnesscery Lines

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, {
-  Component
-} from 'react';
+import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import Home from './components/TodayWorkout/TodayWorkout';
 import Edit from './components/Edit'
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route , Redirect} from "react-router-dom";
 
 function App() { 
   const email = localStorage.getItem('email');
     const isLogged = email==='fatma.o.siam@gmail.com';
       return (
         <Router>
-          <Route path="/login" exact render={() => (!isLogged ? <Login /> : <Home />)} />
+          <Route path="/login" exact render={() => (!isLogged ? <Login /> : <Redirect to='/' />)} />
           <Route path='/'   exact  render={()=>(isLogged ? <Home /> : <Login /> )}/>
           <Route path='/edit'  exact render={()=>(isLogged ? <Edit /> : <Login />)}/>
         </Router>
