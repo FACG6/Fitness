2ed49b180a94b210692dd0f35df62b20993b0c5b
knanahassouna1 <knanahassouna2@gmail.com>
2019-04-07 10:48:20 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Login from './components/Login';
 import Home from './components/TodayWorkout/TodayWorkout';
 import Edit from './components/Edit'
+import { BrowserRouter as Router, Route , Redirect, Switch} from "react-router-dom";
 
-import { BrowserRouter as Router, Route , Redirect} from "react-router-dom";
 
-function App() { 
-  const email = localStorage.getItem('email');
-    const isLogged = email==='fatma.o.siam@gmail.com';
-      return (
-        <Router>
-          <Route path="/login" exact render={() => (!isLogged ? <Login /> : <Redirect to='/' />)} />
-          <Route path='/'   exact  render={()=>(isLogged ? <Home /> : <Login /> )}/>
-          <Route path='/edit'  exact render={()=>(isLogged ? <Edit /> : <Login />)}/>
-        </Router>
-      )  
+
+export default class App extends Component {
+  state = {
+    isLogged: false, 
+    email: '',
+    password: ''
+  }
   
-}
+  handleLogin = () => (this.state.email === '1' && this.state.password === '1' ) ? this.setState({isLogged: true}) : null;
+   
+     
+  handleInput = ({ target: { value, name } }) => this.setState({ [name]: value});
 
-export default App;
+  render() {
+    const { isLogged, email, password } = this.state;
+    return (
+      <div>
+        <Router>
+          <Switch>
+          <Route path="/login" exact render={() => (!isLogged ? 
+          <Login  
+            email={email} 
+            password={password} 
+            handleClick={this.handleLogin} 
+            handleChange={this.handleInput}
+          /> : <Redirect to='/' />)} />
+          <Route path='/'   exact  render={()=>(isLogged ? <Home /> : <Login 
+            email={email} 
+            password={password} 
+            handleClick={this.handleLogin} 
+            handleChange={this.handleInput}/> )}/>
+          <Route path='/edit'  exact render={()=>(isLogged ? <Edit /> : <Login />)}/>
+          </Switch>
+        </Router>
+        
+      </div>
+    )
+  }
+}
