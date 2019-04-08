81538eee7ffa5695db63dd6fedf99180c05bd159
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-08 13:51:21 +0300

    edit style

--- src/components/SharedComponent/icon.jsx
+++ src/components/SharedComponent/icon.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import Button from './button'
 
-const handleclick = (e) => {
+const handlelogout = (e) => {
   sessionStorage.clear();
   window.location.href='/login';
   return true;
@@ -15,7 +15,7 @@ const Icons =()=> {
         <Link to='/'> <div className="icon"><i className="fas fa-home icon" /></div> </Link>
         <Button buttonText=''
           id='logoutbtn'
-          onClick={handleclick}
+          onClick={handlelogout}
           className='fas fa-sign-out-alt logoutbtn'
           type='submit' />
       </div>
