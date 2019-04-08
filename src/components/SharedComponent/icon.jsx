5f94bc2a8698b2721401b7bd61caf322b199df3b
Fatma Siam <fatma.o.siam@gmail.com>
2019-04-08 13:52:34 +0300

    edit style

--- src/components/SharedComponent/icon.jsx
+++ src/components/SharedComponent/icon.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router-dom';
 import Button from './button'
 
 const handlelogout = (e) => {
